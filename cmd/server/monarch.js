52c446e056754bde9ebd0c269c533db910fa1d43
Anh Huynh <huynhduyanh123123@gmail.com>
2026-06-18 12:37:00 +0700

    Update monarch.js

--- cmd/server/monarch.js
+++ cmd/server/monarch.js
@@ -13,10 +13,10 @@ function _() {
 
         operators: [
             '=', '>', '<', '!', '==', '<=', '>=', '!=', '&&', '||', '++', '--',
-            '+', '-', '*', '/', '%', '...'
+            '+', '-', '*', '/', '%', '...', ':'
         ],
 
-        symbols: /\.\.\.|--|\+\+|&&|\|\||==|!=|>=|<=|[=><!+\-*/(){}\[\].,;]/,
+        symbols: /\.\.\.|--|\+\+|&&|\|\||==|!=|>=|<=|[=><!+\-*/(){}\[\]\.:]/,
 
         escapes: /\\(?:[bfnrt\\"']|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
 
@@ -34,8 +34,16 @@ function _() {
                 // Whitespace and comments
                 {include: '@whitespace'},
 
-                // Delimiters and operators
-                [/[{}()\[\]]/, '@brackets'],
+                // Object literals
+                [/\{/, '@brackets', '@object_literal'],
+
+                // Delimiters (comma, semicolon)
+                [/[;,]/, 'delimiter'],
+
+                // Other brackets
+                [/[()\[\]]/, '@brackets'],
+
+                // Operators and symbols
                 [/@symbols/, {
                     cases: {
                         '@operators': 'operator',
@@ -50,11 +58,60 @@ function _() {
                 // 2. Integers
                 [/\d+/, 'number'],
 
-                // Delimiters (comma, semicolon)
-                [/[;,]/, 'delimiter'],
-
                 // Strings
                 [/"/, 'string', '@string'],
+            ],
+
+            object_literal: [
+                // Whitespace and comments
+                {include: '@whitespace'},
+
+                // End of object literal
+                [/\}/, '@brackets', '@pop'],
+
+                // String keys
+                [/"/, 'string.key', '@string_in_object'],
+
+                // Identifiers (keys) and keywords
+                [/[a-zA-Z_][\w_]*/, {
+                    cases: {
+                        '@keywords': 'keyword',
+                        '@literals': 'keyword.literal',
+                        '@default': 'key'
+                    }
+                }],
+
+                // Nested object literals
+                [/\{/, '@brackets', '@object_literal'],
+
+                // Other brackets
+                [/[()\[\]]/, '@brackets'],
+
+                // Delimiters (comma)
+                [/,/, 'delimiter'],
+
+                // Operators and symbols
+                [/@symbols/, {
+                    cases: {
+                        '@operators': 'operator',
+                        '@default': ''
+                    }
+                }],
+
+                // Numbers
+                [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+                [/\d+[eE][\-+]?\d+/, 'number.float'],
+                [/\d+/, 'number'],
+
+                // Strings (value strings - fallback)
+                [/"/, 'string', '@string'],
+            ],
+
+            string_in_object: [
+                [/[^\\"]+/, 'string.key'],
+                [/@escapes/, 'string.escape'],
+                [/\\./, 'string.escape.invalid'],
+                [/"/, 'string.key', '@pop']
             ],
 
             whitespace: [
